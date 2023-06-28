d6d1ab81c14c6d44014c006a7c670caefb14f209
cxk_git <1720921998@qq.com>
2023-06-29 00:33:20 +0800

    init project

---
+++ components/comp-a.vue
@@ -0,0 +1,20 @@
+<template>
+  <view>
+    
+  </view>
+</template>
+
+<script>
+  export default {
+    name:"comp-a",
+    data() {
+      return {
+        
+      };
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
