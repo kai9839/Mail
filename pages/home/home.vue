e47351bb8480d10a9c231e82741d9753772eb5fb
cxk_git <1720921998@qq.com>
2023-06-30 21:19:10 +0800

    swiper

---
+++ pages/home/home.vue
@@ -0,0 +1,50 @@
+<template>
+  <view>
+    <!-- 轮播图区域 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+      <swiper-item v-for="(item, i) in swiperList" :key="i">
+        <view class="swiper-item">
+          <image :src="item.image_src"></image>
+        </view>
+      </swiper-item>
+    </swiper>
+  </view>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        // 1. 轮播图的数据列表
+        swiperList: [],
+      };
+    },
+    onLoad() {
+      // 2. 在小程序页面刚加载的时候，调用获取轮播图数据的方法
+      this.getSwiperList()
+    },
+    methods: {
+      // 3. 获取轮播图数据的方法
+      async getSwiperList() {
+        // 3.1 发起请求
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/home/swiperdata')
+        // 3.2 请求失败
+        if (res.meta.status !== 200) {
+          return uni.showToast({
+            title: '数据请求失败！',
+            duration: 1500,
+            icon: 'none',
+          })
+        }
+        // 3.3 请求成功，为 data 中的数据赋值
+        this.swiperList = res.message
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+
+</style>
