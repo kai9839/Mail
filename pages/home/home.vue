884e9c2f8339380e3231eb0d39c900edfa7319e7
cxk_git <1720921998@qq.com>
2023-07-01 22:51:00 +0800

    home-finish

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -14,6 +14,29 @@
         <image :src="item.image_src" class="nav-img"></image>
       </view>
     </view>
+    <!-- 楼层区域 -->
+    <view class="floor-list">
+      <!-- 楼层 item 项 -->
+      <view class="floor-item" v-for="(item, i) in floorList" :key="i">
+        <!-- 楼层标题 -->
+        <image :src="item.floor_title.image_src" class="floor-title"></image>
+        <!-- 楼层图片区域 -->
+        <view class="floor-img-box">
+          <!-- 左侧大图片的盒子 -->
+          <navigator class="left-img-box" :url="item.product_list[0].url">
+            <image :src="item.product_list[0].image_src" :style="{width: item.product_list[0].image_width + 'rpx'}"
+              mode="widthFix"></image>
+          </navigator>
+          <!-- 右侧 4 个小图片的盒子 -->
+          <view class="right-img-box">
+            <navigator class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0"
+              :url="item2.url">
+              <image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}"></image>
+            </navigator>
+          </view>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -25,6 +48,8 @@
         swiperList: [],
         // 分类导航的数据列表
         navList: [],
+        // 楼层的数据列表
+        floorList: [],
       };
     },
     onLoad() {
@@ -32,6 +57,8 @@
       this.getSwiperList()
       // 调用获取分类导航数据的方法
       this.getNavList()
+      // 在 onLoad 中调用获取楼层数据的方法
+      this.getFloorList()
     },
     methods: {
       // 获取轮播图数据的方法
@@ -61,6 +88,19 @@
             url: '/pages/cate/cate'
           })
         }
+      },
+      // 定义获取楼层列表数据的方法
+      async getFloorList() {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/home/floordata')
+        if (res.meta.status !== 200) return uni.$showMsg()
+        res.message.forEach(floor => {
+          floor.product_list.forEach(prod => {
+            prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
+          })
+        })
+        this.floorList = res.message
       }
     }
   }
@@ -86,5 +126,22 @@
       width: 128rpx;
       height: 140rpx;
     }
+  }
+
+  .floor-title {
+    height: 60rpx;
+    width: 100%;
+    display: flex;
+  }
+
+  .right-img-box {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+
+  .floor-img-box {
+    display: flex;
+    padding-left: 10rpx;
   }
 </style>
