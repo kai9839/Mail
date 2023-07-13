a33f674b25461a20e8c11452d79130e20ce98b68
cxk_git <1720921998@qq.com>
2023-07-13 11:36:33 +0800

    login,payOrder-finish

--- main.js
+++ main.js
@@ -12,17 +12,26 @@ uni.$http = $http
 $http.baseUrl = 'https://api-hmugo-web.itheima.net'
 
 // 请求开始之前做一些事情
-$http.beforeRequest = function(option) {
+$http.beforeRequest = function(options) {
   uni.showLoading({
     title: '数据加载中...',
   })
+
+  // 判断请求的是否为有权限的 API 接口
+  if (options.url.indexOf('/my/') !== -1) {
+    // 为请求头添加身份认证字段
+    options.header = {
+      // 字段的值可以直接从 vuex 中进行获取
+      Authorization: store.state.m_user.token,
+    }
+  }
 }
 
 $http.afterRequest = function() {
   uni.hideLoading()
 }
 
-uni.$showKsg = function(title = '数据加载失败！', duration = 1500) {
+uni.$showMsg = function(title = '数据加载失败！', duration = 1500) {
   uni.showToast({
     title,
     duration,
